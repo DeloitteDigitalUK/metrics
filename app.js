14c057d20d2f5ad25659f000b55387c0b87faf31
Johnathan Phan <johphan@deloitte.co.uk>
2016-06-07 14:30:03 +0100

    playing with page scrape code

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var schedule = require('node-schedule');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -55,11 +59,39 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-var schedule = require('node-schedule');
  
 var j = schedule.scheduleJob('15 * * * *', function(){
-  
+  app.get('/scrape', function(req, res){
+    
+    url = 'http://www.imdb.com/title/tt1229340/';
+
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+
+            var title, release, rating;
+            var json = { title : "", release : "", rating : ""};
+
+            // We'll use the unique header class as a starting point.
+
+            $('.header').filter(function(){
+
+           // Let's store the data we filter into a variable so we can easily see what's going on.
+
+                var data = $(this);
+
+           // In examining the DOM we notice that the title rests within the first child element of the header tag. 
+           // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+                title = data.children().first().text();
+
+           // Once we have our title, we'll store it to the our json object.
+
+                json.title = title;
+            })
+        }
+    })
+})
 });
 
 module.exports = app;
